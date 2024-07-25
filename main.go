25f1bdf19d86a4af240f422bd68d5c842a4d9b51
lovemew67 <lovemew67@gmail.com>
2024-07-25 14:49:54 +0800

    tmp

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/grafana/alloy/internal/alloy/alloycli"
+
 	// Register Prometheus SD components
 	_ "github.com/grafana/loki/v3/clients/pkg/promtail/discovery/consulagent"
 	_ "github.com/prometheus/prometheus/discovery/install"
